83ff0702e5568713d111d6c4410903c03aecc0ed
Paul Ramisch <paul@mcp-pr.fritz.box>
2022-03-03 22:55:22 +0100

    Fix: Cards on the homepage

--- site/snippets/cards.php
+++ site/snippets/cards.php
@@ -9,15 +9,11 @@ $images = $page->images()->sortBy('sort');
     <?php foreach ($images as $image):
     $link = $image->link()->toPage()
     ?>
-    <figure class="nw_card col-6 row">
-        <div class="nw_card-img col-12 col-md-6">
-            <?=($link) ? "<a href='" . $link->url() .  "'>": "" ?><img src="<?= $image->url() ?>" alt="<?= $image->alt() ?>"><?=($link) ? "</a>" : "" ?> 
-        </div>
+    <figure class="nw_card col-6 col-md-3"> 
         <?php if($image->link()->toPage()): ?>
-        <div class="nw_card-info col-12 col-md-6">
-            <div>
-                <?=($link) ? "<a href='" . $link->url() .  "'>": "" ?><h4><?= $link->title() ?></h4><p><?= $image->description() ?></p><?=($link) ? "</a>" : "" ?> 
-            </div>
+        <div class="nw_card-info">
+            <?=($link) ? "<a href='" . $link->url() .  "'>": "" ?><img src="<?= $image->url() ?>" alt="<?= $image->alt() ?>"><?=($link) ? "</a>" : "" ?> 
+            <?=($link) ? "<a href='" . $link->url() .  "'>": "" ?><h4><?= $link->title() ?></h4><p><?= $image->description() ?></p><?=($link) ? "</a>" : "" ?> 
         </div>
         <?php endif ?>
     </figure>
